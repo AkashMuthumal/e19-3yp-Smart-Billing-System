8568ac41060985488c0f86d9c1f6ca72a315493b
Akash Withanaarachchi <e19453@eng.pdn.ac.lk>
2024-01-06 17:41:56 +0530

    bug fix

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 import express from "express";
-import { PORT } from "./config/app.js";
 import cors from "cors";
-
 
 import connection from "./config/database.js";
 // Gives us access to variables set in the .env file via `process.env.VARIABLE_NAME` syntax
@@ -10,14 +8,19 @@ import "dotenv/config";
 import userRouter from "./routes/userRoute.js";
 import productRouter from "./routes/productRoute.js";
 import billRouter from "./routes/billRoute.js";
-import itemPurchasedRoute from "./routes/itemPurchasedRoute.js"
+import itemPurchasedRoute from "./routes/itemPurchasedRoute.js";
 
 const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+    res.send("Hello, this is a simple Express server!");
+});
 
 // Middleware for parsing request body
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors())
+app.use(cors());
 
 // Middleware for handling routes
 app.use("/users", userRouter);
