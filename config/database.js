8824c0af79c00c043835d4a077ffaef214a3a651
Akash Withanaarachchi <e19453@eng.pdn.ac.lk>
2024-01-30 05:40:48 +0530

    bug fix

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
-import "dotenv/config";
+// import "dotenv/config";
 
 // Define your MongoDB connection URI (you can use an environment variable)
 const MONGODB_URI = process.env.MONGODB_URI;
